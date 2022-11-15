473c35f3009cdf3ecb7f0974b6eb0c403ab03f18
Pedro Fialho <fialhopm@gmail.com>
2022-11-14 22:01:10 -0800

    set 1 challenge 8

--- set1/8_detect_aes_ecb_mode.go
+++ set1/8_detect_aes_ecb_mode.go
@@ -1,8 +1,35 @@
 package set1
 
+import "fmt"
+
 // DetectAesEcbMode detects which input buffer has been encrypted with AES-128
 // in ECB mode and returns it.
-func DetectAesEcbMode(data [][]byte) ([]byte, error) {
-
-	return nil, nil
+//
+// The detection algorithm is very naive: a buffer is considered to have been
+// encrypted with ECB iff it contains at least one duplicate 16-byte block.
+func DetectAesEcbMode(data [][]byte) (int, error) {
+	const keySize = 16
+	candidates := make([]int, 0)
+	for i, buffer := range data {
+		memo := make(map[string]struct{})
+		start, end := 0, keySize
+		for end < len(buffer) {
+			block := string(buffer[start:end])
+			if _, ok := memo[block]; ok {
+				candidates = append(candidates, i)
+				break
+			} else {
+				memo[block] = struct{}{}
+			}
+			start = end
+			end += keySize
+		}
+	}
+	if len(candidates) == 0 {
+		return 0, fmt.Errorf("failed to detect AES ECB encrypted ciphertext")
+	}
+	if len(candidates) > 1 {
+		return 0, fmt.Errorf("detected more than one AES ECB encrypted ciphertext")
+	}
+	return candidates[0], nil
 }
--- set1/8_detect_aes_ecb_mode_test.go
+++ set1/8_detect_aes_ecb_mode_test.go
@@ -2,8 +2,22 @@ package set1_test
 
 import (
 	"testing"
+
+	"github.com/fialhopm/cryptopals/set1"
+	"github.com/fialhopm/cryptopals/testutil"
 )
 
 func TestDetectAesEcbMode(t *testing.T) {
-
+	data, err := testutil.ReadAndHexDecode("1_8.txt")
+	if err != nil {
+		t.Fatalf("testutil.ReadAndHexDecode: %v", err)
+	}
+	got, err := set1.DetectAesEcbMode(data)
+	if err != nil {
+		t.Fatalf("set1.DetectAesEcbMode: %v", err)
+	}
+	want := 132
+	if want != got {
+		t.Fatalf("want %#v got %#v", want, got)
+	}
 }
